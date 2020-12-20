63496909f3dd714733e8b844e96ff5576ae58a1f
theandrew168 <steveday168@gmail.com>
2020-12-20 11:53:14 -0600

    Impl basic gradient

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -58,6 +59,36 @@ vec3_mul_scalar(struct vec3 v1, float s)
     return v;
 }
 
+static struct vec3
+vec3_div_scalar(struct vec3 v1, float s)
+{
+    struct vec3 v = {
+        .x = v1.x / s,
+        .y = v1.y / s,
+        .z = v1.z / s,
+    };
+
+    return v;
+}
+
+static float
+vec3_length_squared(struct vec3 v1)
+{
+    return (v1.x * v1.x) + (v1.y * v1.y) + (v1.z * v1.z);
+}
+
+static float
+vec3_length(struct vec3 v1)
+{
+    return sqrtf(vec3_length_squared(v1));
+}
+
+static struct vec3
+vec3_unit(struct vec3 v1)
+{
+    return vec3_div_scalar(v1, vec3_length(v1));
+}
+
 struct ray {
     struct vec3 origin;
     struct vec3 direction;
@@ -69,12 +100,15 @@ ray_at(struct ray r, float t)
     return vec3_add(r.origin, vec3_mul_scalar(r.direction, t));
 }
 
-struct camera {
-    struct vec3 origin;
-    struct vec3 horizontal;
-    struct vec3 vertical;
-    struct vec3 lower_left_corner;
-};
+static struct vec3
+ray_color(struct ray r)
+{
+    struct vec3 unit_direction = vec3_unit(r.direction);
+    float t = 0.5f * (unit_direction.y + 1.0f);
+    struct vec3 black = { 1.0f, 1.0f, 1.0f };
+    struct vec3 color = { 0.5f, 0.7f, 1.0f };
+    return vec3_add(vec3_mul_scalar(black, 1.0f - t), vec3_mul_scalar(color, t));
+}
 
 int
 main(int argc, char* argv[])
@@ -86,23 +120,32 @@ main(int argc, char* argv[])
         }
     }
 
-    struct camera c = {
-        .origin = { 0.0f, 0.0f, 0.0f },
-        .horizontal = { 4.0f, 0.0f, 0.0f },
-        .vertical = { 0.0f, 2.0f, 0.0f },
-        .lower_left_corner = { -2.0f, -1.0f, -1.0f },
-    };
+    float aspect_ratio = 16.0f / 9.0f;
+    long image_width = 1280;
+    long image_height = 720;
 
-    long width = 256;
-    long height = 256;
-    ppm_write_header(stdout, width, height);
-    for (long y = height - 1; y >= 0; y--) {
+    float viewport_width = 16.0f;
+    float viewport_height = 9.0f;
+    float focal_length = 1.0f;
+
+    struct vec3 origin = { 0.0f, 0.0f, 0.0f };
+//    struct vec3 horizontal = { viewport_width, 0.0f, 0.0f };
+//    struct vec3 vertical = { 0.0f, viewport_height, 0.0f };
+    struct vec3 lower_left_corner = { -8.0f, -4.5f, -1.0f };
+
+    ppm_write_header(stdout, image_width, image_height);
+    for (long y = image_height - 1; y >= 0; y--) {
         fprintf(stderr, "lines remaining: %ld\n", y);
-        for (long x = 0; x < width; x++) {
-            float r = (float)x / (width - 1);
-            float g = (float)y / (height - 1);
-            float b = 0.25f;
-            ppm_write_pixel(stdout, r, g, b);
+        for (long x = 0; x < image_width; x++) {
+            float u = (float)x / (image_width - 1);
+            float v = (float)y / (image_height - 1);
+            struct ray r = {
+                .origin = origin,
+                .direction = { -8.0f + u * viewport_width, -4.5f + v * viewport_height, -1.0f },
+            };
+
+            struct vec3 pixel_color = ray_color(r);
+            ppm_write_pixel(stdout, pixel_color.x, pixel_color.y, pixel_color.z);
         }
     }
 
