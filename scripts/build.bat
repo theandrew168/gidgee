7ee4f85f03548b5d9f2ae9183dfa2937716aec11
theandrew168 <steveday168@gmail.com>
2020-12-20 18:36:33 -0600

    Update build.bat with more info

--- scripts/build.bat
+++ scripts/build.bat
@@ -12,9 +12,10 @@ set SOURCES=                ^
 :: /FC  use full pathnames in diagnostics
 :: /MT  link with LIBCMT.LIB
 :: /WL  enable one line diagnostics
+:: /Zi  generate debugging info
 :: /Fo  name object file
 :: /I   add to include search path
-set CFLAGS=/FC /MT /WL /Fo.\src\ /I%cd%\src\ /I%cd%\vendor\include\
+set CFLAGS=/FC /MT /WL /Zi /Fo.\src\ /I%cd%\src\ /I%cd%\vendor\include\
 
 :: /OUT          name of output file
 :: /SUBSYSTEM    type of application (usually CONSOLE or WINDOWS)
@@ -26,12 +27,16 @@ set LDFLAGS=/OUT:.\main.exe /SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup /INCREMENTA
 :: glfw3.lib     open source library for multi-platform OpenGL and Vulkan development
 :: gdi32.lib     primitive drawing functions for display output
 :: kernel32.lib  exposes most of the Win32 base APIs
-:: shell32.lib   misc windows utilties?
+:: shell32.lib   misc windows utilties and assorted things
 :: user32.lib    creates and manipulates standard elements of the Windows UI
 set LDLIBS=/LIBPATH:"%cd%\vendor\lib64\windows\" glfw3.lib gdi32.lib kernel32.lib shell32.lib user32.lib
 
-:: cl.exe  MSVC compiler command-line interface
-:: /link   options to be passed to the linker
+:: cl.exe   MSVC compiler command-line interface
+:: CFLAGS   options for compilation
+:: SOURCES  source files to be compiled
+:: /link    options to be passed to the linker
+:: LDFLAGS  options for linking
+:: LDLIBS   libraries to link against
 cl.exe %CFLAGS% %SOURCES% /link %LDFLAGS% %LDLIBS%
 
 echo Error: %ERRORLEVEL%
